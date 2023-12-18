beb5cfee18ad73ec4767dbb8d5f2d04fe55eec89
prejuk <93695329+prejuk@users.noreply.github.com>
2023-12-17 19:33:01 -0800

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Preju Kanuparthy on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
